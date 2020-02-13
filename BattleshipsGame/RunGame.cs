931da4c2e69ffca744e799e4cf223fc38eb214ea
Dylan <dylan.f.derouchie@gmail.com>
2020-02-13 16:09:38 -0600

    Forgot to add some comments to my methods in the run game class so went ahead and did that.

--- BattleshipsGame/RunGame.cs
+++ BattleshipsGame/RunGame.cs
@@ -22,8 +22,8 @@ namespace BattleshipsGame
         //Methods(CAN DO)
 
 
-        //Game Setup(beginning of the game)
-        private void BeginGame()
+        
+        private void BeginGame()//Game Setup(beginning of the game)
         {
             Console.WriteLine("------------------------------------------------------------");
             Console.WriteLine("|Welcome to my version of the traditional Battleships Game!|");
@@ -34,7 +34,7 @@ namespace BattleshipsGame
             Console.ReadLine();
             HowManyPlayers();
         }
-        private void DisplayRules()
+        private void DisplayRules()//Displays the rules of the game along with general info
         {
             Console.WriteLine("What is Battleships? Well, Battleships is a game where 2 players duke it out with 2 game boards \n" +
                               "in some intense boat on boat combat. Players will flip a coin or choose who gets to go first in \n" +
@@ -45,7 +45,7 @@ namespace BattleshipsGame
                               "who called the shot to mark there board with the respective marker. So lets get into it and \n" +
                               "sink some Battleships! Press (Enter) to Continue...");
         }
-        private void DisplayShips()
+        private void DisplayShips()//Displays the ships used in the game and there slot amounts aka how many times they can be hit
         {
             Console.WriteLine("--------------------------------------------------------------");
             Console.WriteLine("|Here is a list of the 5 ships we will be using in this game |");
@@ -64,7 +64,7 @@ namespace BattleshipsGame
             }
             Console.WriteLine("\nPress (Enter) to Continue...");
         }
-        private void HowManyPlayers()
+        private void HowManyPlayers()//lets user input how many players
         {
             Console.WriteLine("\n---------------------------------------------------------------------------");
             Console.WriteLine("|Now lets pick how many players there are(0 is watching 2 computers play).|");
@@ -99,57 +99,65 @@ namespace BattleshipsGame
             }
             Console.Clear();
         }
-
-
-        //Mid Game(actually playing the game)
-        private void MidGame()
-        {
-
-        }
-        private void PlaceShips()
-        {
-
-        }
-        private void TurnSwitch()
+        private void DecideWhoGoesFirst()//randomly picks who gets to go first
         {
 
         }
 
-        private void Shotchoice()
+
+
+        private void MidGame()//Mid Game(actually playing the game)
         {
 
         }
-            private void CompareOpponentBoardToChoice()
+        private void PlaceShips()//allows both players to place there ships
+        {
+
+        }
+        private void TurnSwitch()//declares which players turn it is
+        {
+
+        }
+
+        private void Shotchoice()//player calls out location they want to hit
+        {
+
+        }
+            private void CompareOpponentBoardToChoice()//compares user input to opponents board to see if its a hit/miss
             {
 
             }
-            private void ConfirmAndMarkBoard()
+            private void ConfirmAndMarkBoard()//marks the players hit/miss board showing spots they have already called
             {
 
             }
-        private void ShipSunkChecker()
+        private void ShipSunkChecker()//checks if the ship has been hit the required amount of times
         {
 
         }
-            private void ShipSunk()
+            private void ShipSunk()//actually sinks the ship removing from the list
             {
 
             }
-
-
-
-        //End Game(compares player boards and ship list and declares a winner)
-        private void EndGame()
+        private void ErrorStatement()//this is just to hold a error statement to call when needed for improper user validation.
         {
 
         }
 
 
-        //Runs Game
-        public void PlayGame()
+
+        private void EndGame()//End Game(compares player boards and ship list and declares a winner)
+        {
+
+        }
+
+
+        
+        public void PlayGame()//Runs Game using the Begin/Mid/End game methods
         {
             BeginGame();
-
+            MidGame();
+            EndGame();
         }
     }
 }
